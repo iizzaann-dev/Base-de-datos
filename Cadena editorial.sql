b0d7d487a3b4688fa4a9079f301d44384da50a58
Izan <iricbla2402@g.educaand.es>
2025-11-21 18:56:04 +0100

    subida de la tarea de los 4 scripts de sql

--- Cadena editorial.sql
+++ Cadena editorial.sql
@@ -4,14 +4,14 @@ create database cadena_editorial;
 
 use cadena_editorial;
 
-create table Sucursales(
+create table sucursales(
 	codigo varchar (10) primary key,
     domicilio varchar (40),
     telefono char (15)
 );
 
 create table periodistas(
-	NIE char (9) primary key,
+	NIF_periodistas char (9) primary key,
     nombre varchar (30), 
     apellidos varchar (50),
     telefono char (15),
@@ -19,14 +19,50 @@ create table periodistas(
 );
 
 create table revistas(
-	n_registro char(30) primary key,
+	n_registro char(40) primary key,
     titulo varchar (50),
     periodicidad varchar (50), 
     tipo varchar(30)
 );
 
 create table empleados(
-	NIF char (9) primary key,
-    nombre varchar (30),
-    apellido varchar 
+	NIF_empleados char (9) primary key,
+    nombre varchar (30) not null,
+    apellido varchar (50) not null,
+    telefono char (15),
+    codigo varchar (10),
+    foreign key (codigo) references sucursales(codigo)
+);
+
+create table publican (
+	codigo varchar(10), 
+    n_registro char (40),
+    primary key (codigo, n_registro),
+    foreign key (codigo) references sucursales(codigo),
+    foreign key (n_registro) references revistas(n_registro)
+    );
+    
+create table escriben (
+	n_registro char (40),
+    NIF_periodistas char (9),
+    primary key (n_registro, NIF_periodistas),
+    foreign key (n_registro) references revistas(n_registro),
+    foreign key (NIF_periodistas) references periodistas(NIF_periodistas)
+);
+
+create table ejemplares (
+	n_registro char (40),
+    fecha date not null,
+    n_paginas int (100),
+    n_ejemplares int(50),
+    primary key (n_registro, fecha),
+	foreign key (n_registro) references revistas(n_registro)
+);
+
+create table secciones (
+	titulo varchar (40),
+    n_registro char(40),
+    extension varchar (30),
+    primary key (titulo, n_registro),
+    foreign key (n_registro) references revistas(n_registro)
 );
---
+++ Empresa de transporte.sql
@@ -0,0 +1,48 @@
+drop database if exists empresa_de_transporte;
+
+create database empresa_de_transporte;
+
+use empresa_de_transporte;
+
+create table camioneros (
+	DNI char (9) primary key,
+    nombre varchar (30),
+    apellidos varchar(50),
+    direccion varchar (50),
+    salario int (5),
+    poblacion varchar (30)
+);
+
+create table camiones (
+	matricula varchar (7) primary key,
+    modelo varchar (20),
+    tipo varchar (20),
+    potencia varchar (20)
+);
+
+create table provincias (
+	codigo_provincia varchar (10) primary key,
+    descripcion varchar (50),
+    destinatario varchar (50),
+    direccion varchar (40)
+);
+
+create table paquetes (
+	codigo_paquetes varchar(10) primary key,
+    descripcion varchar (50),
+    destinatario varchar (50),
+    direccion varchar (40),
+    DNI char (9),
+    codigo_provincia varchar (10),
+    foreign key (DNI) references camioneros (DNI),
+    foreign key (codigo_provincia) references provincias (codigo_provincia)
+);
+
+create table conduce (
+	DNI char (9) not null,
+    matricula char(7),
+    fecha date not null,
+    primary key(DNI, matricula, fecha),
+    foreign key (DNI) references camioneros (DNI),
+    foreign key (matricula) references camiones(matricula)
+);
